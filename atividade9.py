616287414ceda69fe3a30a9e256535d90739a19f
MarceloPrates0 <marcelo0610d@gmail.com>
2025-03-12 19:54:26 +0000

    12/03

--- atividade9.py
+++ atividade9.py
@@ -7,16 +7,16 @@ emprestimo = float(input("Quanto de empréstimo você deseja?: R$"))
 renda_por10 = renda_mensal * 10
 if emprestimo > renda_por10:
     print("Não é possível realizar o empréstimo.")
-    print("Foi possível realizar o empréstimo.")
-
-parcelas = int(input("Em quantas parcelas você irá pagar o empréstimo? "))
-valor_parcela = emprestimo / parcelas
-valor_pagamento= renda_mensal * 0.3
-
-if valor_parcela > valor_pagamento:
-    print("Quantidade inválida de parcelas, escolha uma quantidade que represente ao menos 30% de sua renda mensal por parcela.")
 else:
-    print(f"\nVocê pagará em {parcelas} parcelas") 
-    print(f"Você pagará o total de R${valor_parcela:.2f} por parcela.") 
+    print("Foi possível realizar o empréstimo.")
+    parcelas = int(input("Em quantas parcelas você irá pagar o empréstimo? "))
+    valor_parcela = emprestimo / parcelas
+    valor_pagamento= renda_mensal * 0.3
+
+    if valor_parcela > valor_pagamento:
+        print("Quantidade inválida de parcelas, escolha uma quantidade que represente ao menos 30% de sua renda mensal por parcela.")
+    else:
+        print(f"\nVocê pagará em {parcelas} parcelas") 
+        print(f"Você pagará o total de R${valor_parcela:.2f} por parcela.") 
 
 
